e929900b27e5f433df87dbe6d15538f4999070cb
Nicolaas Matthijs <nicolaas.matthijs@caret.cam.ac.uk>
2013-10-14 19:59:57 +0100

    Follow up on duplicate user creation

--- shared/oae/api/oae.api.user.js
+++ shared/oae/api/oae.api.user.js
@@ -90,7 +90,7 @@ define(['exports', 'jquery', 'underscore'], function(exports, $, _) {
                 callback(null, data);
             },
             'error': function(jqXHR, textStatus) {
-                callback({'code': jqXHR.status, 'msg': jqXHR.statusText});
+                callback({'code': jqXHR.status, 'msg': jqXHR.responseText});
             }
         });
     };
@@ -115,7 +115,7 @@ define(['exports', 'jquery', 'underscore'], function(exports, $, _) {
                 callback(null, data);
             },
             'error': function(jqXHR, textStatus) {
-                callback({'code': jqXHR.status, 'msg': jqXHR.statusText});
+                callback({'code': jqXHR.status, 'msg': jqXHR.responseText});
             }
         });
     };
@@ -152,9 +152,8 @@ define(['exports', 'jquery', 'underscore'], function(exports, $, _) {
                 callback(null, data);
             },
             'error': function(jqXHR, textStatus) {
-                callback({'code': jqXHR.status, 'msg': jqXHR.statusText});
+                callback({'code': jqXHR.status, 'msg': jqXHR.responseText});
             }
         });
     };
-
 });
